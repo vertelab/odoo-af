5386f1f9849008831767b2705214a0598c8cba78
Daniel Eriksson <daniel.eriksson@vertel.se>
2020-03-19 13:33:23 +0000

    af_rest_appointment: removed unnecceary import

--- af_rest_appointment/models/af_appointment.py
+++ af_rest_appointment/models/af_appointment.py
@@ -25,7 +25,6 @@ from urllib import request
 from urllib.error import URLError, HTTPError
 import json
 import logging
-import re
 from odoo.exceptions import Warning
 
 _logger = logging.getLogger(__name__)
