5b10f71148471fde21cbe7e5d0e9a9c6da099d12
Daniel Eriksson <daniel.eriksson@vertel.se>
2020-03-20 10:51:49 +0000

    calendar_slot: new module for handling schedules/"slots" from teleoptic

---
+++ calendar_slot/models/calendar_slot.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class CalendarSlot(models.Model):
+    _name = 'calendar.slot'
+    _description = "Slot"
+
+    name = fields.Char(string='Slot name', required=True)
+    start = fields.Datetime(string='Start', required=True, help="Start date of a slot")
+    stop = fields.Datetime(string='Stop', required=True, help="Stop date of a slot")
+    scheduled_agents = fields.Integer(string='Scheduled agents', help="Number of scheduled agents")
+    forecasted_agents = fields.Integer(string='Forecasted agents', help="Number of forecasted agents")
+    competence = fields.Many2one(string='Competence', comodel_name='calendar.slot.competence', help="Related competence")
+
+class CalendarSlotCompetence(models.Model):
+    # TODO: This class should be merged with a generic "competence"-class if we use it in more areas of odoo.
+    _name = 'calendar.slot.competence'
+    _description = "Competence"
+
+    name = fields.Char('Competence name', required=True)
+    # AF specific attribute
+    ipf_id = fields.Char('IPF Id', required=True, help="The IPF competence id, if this is wrong the integration won't work")
