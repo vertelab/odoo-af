20cbd91300900e2d90d71e578763433529eb147d
Daniel Eriksson <daniel.eriksson@vertel.se>
2020-03-26 12:21:54 +0000

    af_rest_appointment: started implementing get_appointments() calendar_booking: new module for bookings

--- af_rest_appointment/models/af_appointment.py
+++ af_rest_appointment/models/af_appointment.py
@@ -36,20 +36,37 @@ _logger = logging.getLogger(__name__)
 class AfAppointment(models.Model):
     _name = "af.appointment"
 
-    def _generate_tracking_id(af_system_id, af_environment):
+    def _generate_tracking_id(self, af_system_id, af_environment):
         tracking_number = datetime.now().strftime("%y%m%d%H%M%S")
-        return "%s-%s-%s" % (af_system_id.upper(), af_environment.upper(), tracking_number)
+        tracking_id = "%s-%s-%s" % (af_system_id.upper(), af_environment.upper(), tracking_number)
+        return tracking_id
 
+    def _generate_ctx(self, is_remote):
+        ctx = ssl.create_default_context()
+        if is_remote:
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+        else:
+            pass # TODO: mTSL here?
+        return ctx
+
+    def _generate_headers(self, af_environment, af_system_id, af_tracking_id):
+        get_headers = {
+            'AF-Environment': af_environment,
+            'AF-SystemId': af_system_id,
+            'AF-TrackingId': af_tracking_id,
+        }
+        return get_headers
 
     # /appointments
-    def get_appointments(self, user_id, from_date, to_date, appointment_types, status_list):
+    def get_appointments(self, user, pnr, from_date, to_date, appointment_types, status_list):
         client_id = self.env['ir.config_parameter'].sudo().get_param('af_rest.client_id')
         client_secret = self.env['ir.config_parameter'].sudo().get_param('af_rest.client_secret')
         af_environment = self.env['ir.config_parameter'].sudo().get_param('af_rest.af_environment')
         af_port = self.env['ir.config_parameter'].sudo().get_param('af_rest.ipf_port')
         af_url = self.env['ir.config_parameter'].sudo().get_param('af_rest.ipf_url')
         af_system_id = self.env['ir.config_parameter'].sudo().get_param('af_rest.af_system_id')
-        
+
         if not (af_url or af_port or client_id or client_secret or af_environment or af_system_id):
             raise Warning('Please setup AF integrations')
 
@@ -58,8 +75,8 @@ class AfAppointment(models.Model):
 
         # Define base url
         # ex: https://ipfapi.arbetsformedlingen.se:443/appointments/v1/resource-planning/competencies/schedules?from_date=2020-03-17T00:00:00Z&client_id=XXXXXXXXX&client_secret=XXXXXXXXX&to_date=2020-03-25T00:00:00Z&competence_id=ded72445-e5d3-4e21-a356-aad200dac83d
-        base_url = "{url}:{port}/{path}?client_id={client}&client_secret={secret}&from_date={from_date_str}&to_date={to_date_str}&user_id={user_id}"
-        
+        base_url = "{url}:{port}/{path}?client_id={client}&client_secret={secret}&from_date={from_date_str}&to_date={to_date_str}{user_str}{pnr_str}&appointment_types={appointment_types_str}&status_list={status_list_str}"
+
         # Insert values into base_url
         get_url = base_url.format(
             url = af_url, # https://ipfapi.arbetsformedlingen.se
@@ -69,31 +86,50 @@ class AfAppointment(models.Model):
             secret = client_secret, # check in anypoint for example
             from_date_str = from_date.strftime("%Y-%m-%d"), # 2020-03-17T00:00:00Z
             to_date_str = to_date.strftime("%Y-%m-%d"), # 2020-03-25T00:00:00Z
-            user_id = 'eridd' # 'eridd' # TODO: fix
+            # user_id = user.id, # 'eridd' # TODO: enable this
+            user_str = ("&user_id=%s" % user) if user else '', # 'eridd'
+            pnr_str = ("&pnr=%s" % pnr) if pnr else '', # '16280810XXXX'
+            appointment_types_str = appointment_types, # TODO: implement better, expected: comma seperated list.
+            status_list_str = status_list, # TODO: implement better, expected: comma seperated list.
         )
         
         # Generate headers for our get
-        get_headers = {
-            'AF-Environment': af_environment,
-            'AF-SystemId': af_system_id,
-            'AF-TrackingId': af_tracking_id,
-        }
+        get_headers = self._generate_headers(af_environment, af_system_id, af_tracking_id)
 
         # Build our request using url and headers
         # Request(url, data=None, headers={}, origin_req_host=None, unverifiable=False, method=None)
         req = request.Request(url=get_url, headers=get_headers)
 
-        ctx= ''
-        #TODO: only use this code in remote tests
-        # tell odoo to ignore cert
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
+        ctx = self._generate_ctx(True) # TODO: change to False
 
         # send GET and read result
         res_json = request.urlopen(req, context=ctx).read()
         # Convert json to python format: https://docs.python.org/3/library/json.html#json-to-py-table 
         res = json.loads(res_json)
+
+        # get list of appointments
+        appointments = res.get.("appointments")
+        # loop over list
+        for appointment in appointments:
+            pass
+            # appointment.get('appointment_channel')
+            # appointment.get('appointment_date')
+            # appointment.get('appointment_end_time')
+            # appointment.get('appointment_start_time')
+            # appointment.get('appointment_length')
+            # appointment.get('appointment_title')
+            # appointment.get('appointment_type')
+            # appointment.get('customer_id')
+            # appointment.get('customer_name')
+            # appointment.get('employee_name')
+            # appointment.get('employee_phone')
+            # appointment.get('employee_signature')
+            # appointment.get('id')
+            # appointment.get('office_address')
+            # appointment.get('location_code')
+            # appointment.get('office_code')
+            # appointment.get('office_name')
+            # appointment.get('status')
 
     # /resource-planning/competencies/schedules
     def get_schedules(self, from_datetime, to_datetime, competences):
@@ -103,7 +139,7 @@ class AfAppointment(models.Model):
         af_port = self.env['ir.config_parameter'].sudo().get_param('af_rest.ipf_port')
         af_url = self.env['ir.config_parameter'].sudo().get_param('af_rest.ipf_url')
         af_system_id = self.env['ir.config_parameter'].sudo().get_param('af_rest.af_system_id')
-        
+
         if not (af_url or af_port or client_id or client_secret or af_environment or af_system_id):
             raise Warning('Please setup AF integrations')
 
@@ -118,7 +154,7 @@ class AfAppointment(models.Model):
         # Define base url
         # ex: https://ipfapi.arbetsformedlingen.se:443/appointments/v1/resource-planning/competencies/schedules?from_date=2020-03-17T00:00:00Z&client_id=XXXXXXXXX&client_secret=XXXXXXXXX&to_date=2020-03-25T00:00:00Z&competence_id=ded72445-e5d3-4e21-a356-aad200dac83d
         base_url = "{url}:{port}/{path}?client_id={client}&client_secret={secret}&from_date={from_date}&to_date={to_date}{comps}"
-        
+
         # Insert values into base_url
         get_url = base_url.format(
             url = af_url, # https://ipfapi.arbetsformedlingen.se
@@ -130,30 +166,21 @@ class AfAppointment(models.Model):
             to_date = to_datetime.strftime("%Y-%m-%dT%H:%M:%SZ"), # 2020-03-25T00:00:00Z
             comps = comp, # &competence_id=ded72445-e5d3-4e21-a356-aad200dac83d
         )
-        
+
         # Generate headers for our get
-        get_headers = {
-            'AF-Environment': af_environment,
-            'AF-SystemId': af_system_id,
-            'AF-TrackingId': af_tracking_id,
-        }
+        get_headers = self._generate_headers(af_environment, af_system_id, af_tracking_id)
 
         # Build our request using url and headers
         # Request(url, data=None, headers={}, origin_req_host=None, unverifiable=False, method=None)
         req = request.Request(url=get_url, headers=get_headers)
 
-        ctx= ''
-        #TODO: only use this code in remote tests
-        # tell odoo to ignore cert
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
+        ctx= ctx = self._generate_ctx(True) # TODO: change to False
 
         # send GET and read result
         res_json = request.urlopen(req, context=ctx).read()
         # Convert json to python format: https://docs.python.org/3/library/json.html#json-to-py-table 
         res = json.loads(res_json)
-        
+
         # Create calendar.schedule from res
         # res: list of dicts with list of schedules
         # schedules: list of dicts of schedules
@@ -182,12 +209,8 @@ class AfAppointment(models.Model):
                         'start': start_time,
                         'stop': stop_time,
                         'duration': 30.0,
-                        'scheduled_agents': schedule.get('scheduled_agents'), # number of agents supposed to be available for this
-                        'forecasted_agents': schedule.get('forecasted_agents'), # May be implemented at a later date.
+                        'scheduled_agents': int(schedule.get('scheduled_agents')), # number of agents supposed to be available for this. Can sometimes be float.
+                        'forecasted_agents': int(schedule.get('forecasted_agents')), # May be implemented at a later date. Can sometimes be float.
                         'competence': competence_id,
                     }
                     self.env['calendar.schedule'].create(vals)
-        
-        # except:
-        #     _logger.error('Appointment url error.')
-
---
+++ calendar_booking/models/calendar_booking.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class CalendarSchedule(models.Model):
+    _name = 'calendar.booking'
+    _description = "Booking"
+
+    name = fields.Char(string='Schedule name', required=True)
+    start = fields.Datetime(string='Start', required=True, help="Start date of a schedule")
+    stop = fields.Datetime(string='Stop', required=True, help="Stop date of a schedule")
+    duration = fields.Float('Duration')
+    user_id = fields.Many2one(string='Case worker', comodel_name='res.user', help="Booked case worker")
+    partner_id = fields.Many2one(string='Case worker', comodel_name='res.partner', help="Booked customer")
