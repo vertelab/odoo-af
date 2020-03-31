bcb261f361f998e6919e9c024af355826d153a06
Sebastian Gieser <sebgi@odoo12.network4>
2020-03-31 09:25:00 +0000

    updated version number

--- calendar_af/__manifest__.py
+++ calendar_af/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Calendar AF",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
