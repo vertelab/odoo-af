49d7d598d33530b8a28436c115f7d1587fbe6d6c
Daniel Eriksson <daniel.eriksson@vertel.se>
2020-04-17 08:54:03 +0000

    test_data_af: new module for importing test data.

---
+++ test_data_af/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Test Data AF",
+    "version": "12.0.1.0.1",
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": ["contacts", "hr", "calendar", "partner_employee360", ],
+    "data": [
+        "data/res.partner.csv",
+    ],
+    "application": False,
+    "installable": True,
+}
