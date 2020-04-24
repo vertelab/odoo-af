b595f2e9e9a662d5b2583bfb2e09e6fda67587f8
Lisa <lisa@odoo12.network1>
2020-04-24 09:35:02 +0000

    af_install_modules: modules installs relevent modules

---
+++ af_install_modules/__manifest__.py
@@ -0,0 +1,26 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Af Install all modules",
+    "version": "12.0.1.0.1",
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": [
+		"contacts", 
+		"hr", 
+		"calendar", 
+		"partner_employee360", 
+		"af_backend_tema", 
+		"partner_kpi_data", 
+		"res_drivers_license",
+		"res_sni",
+		"res_ssyk",
+		"res_sun",
+		"partner_daily_notes",
+		"partner_desired_jobs",
+			],
+    "application": False,
+    "installable": True,
+}
