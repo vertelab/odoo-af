2a9c754b1032aaae84380782253d3b4463adde01
Fredrik Arvas <fredrik@arvas.se>
2020-08-29 07:39:50 +0200

    Små ändringar i översättning och rubriker

--- hr_360_view/__manifest__.py
+++ hr_360_view/__manifest__.py
@@ -1,8 +1,11 @@
 {
     'name': 'HR 360 View',
-    'version': '12.0.0.1',
+    'version': '12.0.0.2',
     'category': 'Human resources',
-    'description': " AFC-667 - HR 360 View Module.",
+      "description": """
+	 v12.0.0.1 AFC-667 - HR 360 View Module. Lägger till huvudsida för handläggare med sökyta samt flikar med uppgifter centrerade från handläggaren.
+	 v12.0.0.2 AFC-713 - Bytt namn från Handläggaryta till Arbetsyta enligt beslut från Införandegruppen. Dolt menyn HR 360.
+    """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'website': 'http://www.vertel.se',
