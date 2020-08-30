e4cd94db727a14a00efa9db2b262d5ae96840533
Robin Chatfield <robin.chatfield@vertel.se>
2020-08-30 19:53:03 +0200

    af_security_rules AFC-137: More rules added. Improved BHTJ connection.

--- af_security_rules/__manifest__.py
+++ af_security_rules/__manifest__.py
@@ -18,7 +18,7 @@
     "data": [
         "security/af_security.xml",
         "security/ir.model.access.csv",
-        #"data/data.xml",
+        "data/data.xml",
     ],
     "application": True,
     "installable": True,
--- hr_360_view/models/hr.py
+++ hr_360_view/models/hr.py
@@ -5,22 +5,18 @@ class HR(models.Model):
     _inherit = 'hr.employee'
 
     related_partner_id = fields.Many2one('res.partner', string="Related Partner", related='user_id.partner_id')
-    
-    jobseekers_ids = fields.One2many('res.partner', inverse_name="office", compute='_get_records')
-    case_ids = fields.One2many('res.partner.case', inverse_name="partner_id", compute='_get_records')
-    daily_note_ids = fields.One2many('res.partner.notes', inverse_name="partner_id", compute='_get_records')
+    jobseekers_ids = fields.One2many('res.partner', compute='_get_records')
+    case_ids = fields.One2many('res.partner.case', compute='_get_records')
+    daily_note_ids = fields.One2many('res.partner.notes', compute='_get_records')
 
     @api.depends('user_id')
     def _get_records(self):
+        _logger.warn('hepp!')
         for rec in self:
-            res_partners_record = rec.env['res.partner'].search([('user_id', '=', self.env.uid)])
-            rec.jobseekers_ids = res_partners_record
-
-            case_id_record = rec.env['res.partner.case'].search([('administrative_officer', '=', self.env.uid)])
-            rec.case_ids = case_id_record
-
-            daily_note_record = rec.env['res.partner.notes'].search([('administrative_officer', '=', self.env.uid)])
-            rec.daily_note_ids = daily_note_record
+            if rec.user_id:
+                rec.jobseekers_ids = rec.env['res.partner'].search([('user_id', '=', rec.user_id.id)])
+                rec.case_ids = rec.env['res.partner.case'].search([('administrative_officer', '=', rec.user_id.id)])
+                rec.daily_note_ids = rec.env['res.partner.notes'].search([('administrative_officer', '=', rec.user_id.id)])
 
     @api.multi
     def search_jobseeker_action(self):
