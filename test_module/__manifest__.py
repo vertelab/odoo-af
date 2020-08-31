e9f8c483ed8ebadbd2c2af7cb9c66cc987df7c4f
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-08-31 15:21:11 +0200

    test_module: made a test module with basic inherit, view inherit, menus, views, fields, button and server action

---
+++ test_module/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Test Module",
+    "version": "12.0.1.0.1",
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    'description': """
+Test Module
+===============================================================================
+Hover over fields to se a brief description of them
+For more information make sure you are in debug mode
+""",
+    "depends": [
+        'contacts'
+    ],
+    "external_dependencies": [
+    ],
+    "data": [
+        'views/res_partner.xml'
+    ],
+    "application": False,
+    "installable": True,
+}
