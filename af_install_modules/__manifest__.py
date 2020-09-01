dda27685bc9d737f9688f5f705ff8665b87f1527
Daniel Eriksson <daniel.eriksson@arbetsformedlingen.se>
2020-09-01 10:04:50 +0200

    af_install_modules AFC-731: fixed mix of tabs and spaces in manifest

--- af_install_modules/__manifest__.py
+++ af_install_modules/__manifest__.py
@@ -4,40 +4,40 @@
     "name": "Af Install all modules",
     "version": "12.0.1.2",
     "author": "Vertel AB",
-	"description": """
-		This module installs all AF-modules at one go.\n
-		Please see the depend-tab (with debug-mode) for a list\n
-		\n
-		 v12.0.1.2  - changed version number to four digits\n
-		 \n
+    "description": """
+        This module installs all AF-modules at one go.\n
+        Please see the depend-tab (with debug-mode) for a list\n
+        \n
+         v12.0.1.2  - changed version number to four digits\n
+         \n
     """,
     
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     "category": "Tools",
     "depends": [
-		"af_backend_tema", 
-		"contacts", 
-		"partner_firstname", 
-		"hr", 
-		"hr_org_chart",
-		"calendar", 
-		"partner_view_360", 
-		"partner_kpi_data", 
-		"res_drivers_license",
-		"res_sni",
-		"res_ssyk",
-		"res_sun",
-		"partner_daily_notes",
-		"partner_desired_jobs",
-		"partner_af_case",
-		"hr_360_view", 
-		#"test_data_demo_sv",
-		#"edi_af_appointment",
-		#"calendar_af",
-		#"help_online",
-		#"af_security",
-			],
+        "af_backend_tema", 
+        "contacts", 
+        "partner_firstname", 
+        "hr", 
+        "hr_org_chart",
+        "calendar", 
+        "partner_view_360", 
+        "partner_kpi_data", 
+        "res_drivers_license",
+        "res_sni",
+        "res_ssyk",
+        "res_sun",
+        "partner_daily_notes",
+        "partner_desired_jobs",
+        "partner_af_case",
+        "hr_360_view", 
+        #"test_data_demo_sv",
+        #"edi_af_appointment",
+        #"calendar_af",
+        #"help_online",
+        #"af_security",
+            ],
     "application": False,
     "installable": True,
 }
