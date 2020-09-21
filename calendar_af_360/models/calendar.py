e42737a9eca90124f96985104af61b6d96a6ade0
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-09-21 17:36:15 +0200

    calendar_af_360 AFC-800, AFC-1071: upcoming meetings now first tab on jobseekers, book button send back to jobseeker form. Very buggy not working

---
+++ calendar_af_360/models/calendar.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import models, fields, api, _
+import logging
+from datetime import datetime
+
+_logger = logging.getLogger(__name__)
+
+class CalendarAppointmentSuggestion(models.Model):
+    _inherit = 'calendar.appointment.suggestion'
+    
+    
+    @api.multi
+    def select_suggestion(self):
+        # check state of appointment
+        super(CalendarAppointmentSuggestion, self).select_suggestion()
+        
+        return{
+            'name': _("Search Jobseekers"),
+            'view_type': 'form',
+            'res_model': "hr.employee.jobseeker.search.wizard",
+            'view_id': False, 
+            'view_mode':"form",
+            'type': 'ir.actions.act_window',
+        }
