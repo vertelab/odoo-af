4f8c0af901217c2820a397fe127376432fd09a7f
Chamster <chamster@89-37.wt.ams.se>
2020-10-08 12:12:21 +0200

    init init

---
+++ af_statistics/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "AF Backend Tema V12",
+    "summary": "AF Backend Theme V12	",
+    "version": "12.0.0.4",
+    "category": "Theme/Backend",
+    "description": """
+		AF Backend tema för Odoo 12.0 community edition.
+		v12.0.0.3 Added dependancy for the web-module.
+    """,
+    "author": "Vertel AB",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'base', 'portal', 'web'
+    ],
+    'qweb': [
+        'static/src/xml/web.xml',
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/template.xml',
+        'views/webclient_template.xml'
+    ]
+}
