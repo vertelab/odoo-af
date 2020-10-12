32fb7d7a46453f9b8a05aee7e1cad552c591d282
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-10-12 15:44:37 +0530

    [ADD] Updated manifest file.

--- af_backend_tema/__manifest__.py
+++ af_backend_tema/__manifest__.py
@@ -9,7 +9,7 @@
     "description": """
 		AF Backend tema för Odoo 12.0 community edition.
 		v12.0.0.3 Added dependancy for the web-module.\n
-		v12.0.0.4 KPD-684 - Increased width of smart-buttons\n
+		v12.0.0.5 KPD-684 - Increased width of smart-buttons\n
     """,
     "author": "Vertel AB",
     "license": "LGPL-3",
