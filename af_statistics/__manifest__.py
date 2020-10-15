b1f0f982253e1a5eff59debaef8379547916551a
Chamster <chamster@89-37.wt.ams.se>
2020-10-15 10:09:34 +0200

    Working level-1 Matomo reporting without grooming.

--- af_statistics/__manifest__.py
+++ af_statistics/__manifest__.py
@@ -2,26 +2,14 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "AF Backend Tema V12",
-    "summary": "AF Backend Theme V12	",
-    "version": "12.0.0.4",
-    "category": "Theme/Backend",
-    "description": """
-		AF Backend tema för Odoo 12.0 community edition.
-		v12.0.0.3 Added dependancy for the web-module.
-    """,
-    "author": "Vertel AB",
-    "license": "LGPL-3",
+    "name": "AF Statistics",
+    "summary": "AF Statistics for Matomo",
+    "version": "12.0.0.1",
+    "category": "TBD",
+    "description": "AF Statistics for Matomo",
+    "author": "AF",
+    "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        'base', 'portal', 'web'
-    ],
-    'qweb': [
-        'static/src/xml/web.xml',
-    ],
-    "data": [
-        'views/assets.xml',
-        'views/template.xml',
-        'views/webclient_template.xml'
-    ]
+    "depends": ["web"],
+    "data": [ "views/assets.xml" ]
 }
