2d383857d8df7f849dc381fd0e978e4b6e913476
Chamster <chamster@89-37.wt.ams.se>
2020-10-20 09:45:47 +0200

    Init

--- af-a11y/__manifest__.py
+++ af-a11y/__manifest__.py
@@ -2,11 +2,11 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "AF Statistics",
-    "summary": "AF Statistics for Matomo",
+    "name": "AF A11Y",
+    "summary": "AF Accessibility",
     "version": "12.0.0.1",
     "category": "TBD",
-    "description": "AF Statistics for Matomo",
+    "description": "Accessibility adaptations and customizations",
     "author": "AF",
     "license": "AGPL-3",
     "installable": True,
