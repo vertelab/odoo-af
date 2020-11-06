139c53c7fde1bcd67c87214fe7d8d3a960d0fcef
Sebastian Gieser <sebgi@odoo12.network4>
2020-11-06 10:17:08 +0000

    af_data_ais-f_loader: disabled jobseeker loading, af_data_jobseeker_api_loader now should load jobseekers

---
+++ af_data_jobseeker_api_loader/__manifest__.py
@@ -0,0 +1,37 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "AIS-F Data Loader",
+    "version": "12.0.1.3",
+    "description": """
+
+AIS-F Data Loader
+==========================================
+Loads database dumps from AIS-F into odoo \n
+Database dump files must be located in AIS-F/filename.csv in 'data_dir' directory defined in odoo.conf\n
+There are test dump files located in data/test_dumps\n
+v12.0.1.2  - added version -explanation\n
+v12.0.1.3  - removed state-partner.csv\n
+\n
+\n
+""",
+
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": [ 
+        "hr", 
+        "calendar", 
+        "hr_employee_firstname_extension", 
+        #"partner_kpi_data", 
+		"partner_daily_notes", 
+		#"partner_desired_jobs",
+    ],
+    "data": [
+        #"data/res.country.state.csv",
+        "data/res_partner.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
