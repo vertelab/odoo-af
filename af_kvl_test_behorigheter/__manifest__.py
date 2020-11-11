58f8144003bf3f700243ac0a45b5813c87b60e7f
Aden Hassan <hasad@89-35.wt.ams.se>
2020-11-11 17:47:18 +0100

    AFC-1187-kvl_behorigheter_Fenix_02: första utkast av behörigheterna i enlighet med gällande grupper

---
+++ af_kvl_test_behorigheter/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "AF-KVL Security",
+    "version": "12.0001",
+    "author": "Aden Hassan",
+    "license": "AGPL-3",
+    "description": "Åtkomsträttigheter inom kvl.",
+    "website": "https://vertel.se/",
+    "category": "Security",
+    "depends": [
+        ],
+    "data": [
+        'security/security.xml',
+    ],
+    "application": False,
+    "installable": True,
+}
