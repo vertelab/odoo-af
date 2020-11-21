8bc8f7597540727e54f15f02c201b89820468edc
Fredrik Arvas <fredrik@arvas.se>
2020-11-21 21:00:38 +0100

    Update __manifest__.py
    with version number

--- af_core_menu_disabling/__manifest__.py
+++ af_core_menu_disabling/__manifest__.py
@@ -7,11 +7,13 @@
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     'category': 'Tools',
-    'version': '12.0.1.1',
+    'version': '12.0.1.2',
     'depends': ['calendar','website','contacts'],
     "description": """
         This module adds a hides core menues for users in the group internal-users.\n
         It also adds a group to Show the hidden menues. \n
+        v12.0.1.2 - removed website-menu, since it will not be part of release 1.0 \n
+        \n
     """,
     'data': [
         'security/security_view.xml',
