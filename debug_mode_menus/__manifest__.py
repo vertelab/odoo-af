6fb37e45bc4883db33ef49bac4043ca80f35e77a
Fredrik Arvas <fredrik@arvas.se>
2020-11-21 17:39:18 +0100

    the module is replace by af_core_menu_disabling

--- debug_mode_menus/__manifest__.py
+++ debug_mode_menus/__manifest__.py
@@ -1,8 +1,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Debug Mode Menus",
-    "version": "12.0.1.0.1",
+    "name": "Depreciated Debug Mode Menus",
+    "version": "12.0.1.0.2",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
@@ -12,12 +12,17 @@
         "contacts",
         "website",
     ],
+    "description": """
+        This module did some hiding of menues based on the debug-setting.\n
+        It is replaced by the module af_core_menu_disabling\n
+    """,
+    
     "external_dependencies": [
     ],
     "data": [
         'views/debug_mode_menu.xml',
 
     ],
-    "application": True,
-    "installable": True,
+    "application": False,
+    "installable": False,
 }
