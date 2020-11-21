2dc0f0ebab401c7f84b11ab76891fec2cfa2d5fe
Fredrik Arvas <fredrik@arvas.se>
2020-11-21 17:53:20 +0100

    Updated the manifest with description

--- af_core_menu_disabling/__manifest__.py
+++ af_core_menu_disabling/__manifest__.py
@@ -2,13 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': "Disable access for core menus",
-    'summary': "Hide some specific menus for internal type of user.",
+    'summary': "Hides core menus for internal users.",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     'category': 'Tools',
-    'version': '12.0.1.0',
+    'version': '12.0.1.1',
     'depends': ['calendar','website','contacts'],
+    "description": """
+        This module adds a hides core menues for users in the group internal-users.\n
+        It also adds a group to Show the hidden menues. \n
+    """,
     'data': [
         'security/security_view.xml',
     ],
