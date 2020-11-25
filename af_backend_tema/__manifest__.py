b823274bcdf0d0be940ac63c0b736f08c156d25a
Fredrik Arvas <fredrik@arvas.se>
2020-11-25 07:46:41 +0100

    Added Depreciated to the manifest

--- af_backend_tema/__manifest__.py
+++ af_backend_tema/__manifest__.py
@@ -2,16 +2,17 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "AF Backend Tema V12",
+    "name": "Depreciated AF Backend Tema V12",
     "summary": "AF Backend Theme V12	",
-    "version": "12.0.0.5",
+    "version": "12.0.0.6",
     "category": "Theme/Backend",
     "description": """
 		AF Backend tema för Odoo 12.0 community edition.
+		This theme is replaced by web_backend_theme
     """,
     "author": "Vertel AB",
     "license": "LGPL-3",
-    "installable": True,
+    "installable": False,
     "depends": [
         'base', 'portal', 'web'
     ],
