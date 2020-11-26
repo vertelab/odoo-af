b3256122ba37bd013df219cfe94535b9eb3e893a
Fredrik Arvas <fredrik@arvas.se>
2020-11-26 10:07:31 +0100

    Update __manifest__.py

--- af_install_modules_dafa_mvp2/__manifest__.py
+++ af_install_modules_dafa_mvp2/__manifest__.py
@@ -18,16 +18,15 @@
 	"website": "https://vertel.se/",
 	"category": "Tools",
 	"depends": [
-        # These modules needs to be installed for  MVP2 functionality
-        # 
-        # 
-        #"af_sale_orders",
+	# These modules needs to be installed for  MVP2 functionality
+	#
+	#"af_sale_orders",
 	#"af_sales_report",
 	"hr_af_holidays",
 	#"send_mail_nadim",
 	#"mail_oe_chatter_user_groups",
 	#"project_jobseeker_views",
-	#"api_project_odoo_xmlrpc", 		        
+	#"api_project_odoo_xmlrpc",
 	],
 	"application": False,
 	"installable": True,
