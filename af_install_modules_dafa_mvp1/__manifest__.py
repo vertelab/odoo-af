cebe8a493607381e15d2353b22eafcb1be7691fb
Fredrik Arvas <fredrik@arvas.se>
2020-11-27 02:53:34 +0100

    Fixed typo

--- af_install_modules_dafa_mvp1/__manifest__.py
+++ af_install_modules_dafa_mvp1/__manifest__.py
@@ -20,7 +20,7 @@
     "website": "https://vertel.se/",
     "category": "Tools",
     "depends": [
-        # These modules needs to be installed for  MVP2 functionality
+        # These modules needs to be installed for  MVP1 functionality
         "auth_saml_ol_groups",
         "auth_saml_af",
         "audit_logger", 
@@ -31,7 +31,7 @@
 	# "partner_mq_ipf", This should not be installed automatically, since it consumes a liste of changes
         "edi_af_aisf_rask_get_jobseeker",
         "edi_af_aisf_rask",
-        # "edi_af_aisf_trask", #field signature does not exist
+        # "edi_af_aisf_trask", #field signature does not exist in partner_daily_notes
         "edi_af_facility",
         "edi_af_krom_postcode",
         "edi_af_officer",
@@ -39,6 +39,7 @@
 	"hr_employee_lastnames",
 	"hr_employee_legacy_id", #OCA
         "hr_employee_views_fenix",
+	#"hr_departments_partner" #AFC-1329
 	"hr_org_chart",
         # "web_a11y_filter_view",   
 	# "web_a11y_report",  
