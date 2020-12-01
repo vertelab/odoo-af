c16bb9a7ff8b4884dcebb057edd1d9a04233b17d
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-12-01 14:15:07 +0100

    calendar_af, calendar_af_360: moved hr_operation_view back to calendar_af, made calendar_af dependent on hr_office

--- calendar_af/__manifest__.py
+++ calendar_af/__manifest__.py
@@ -14,7 +14,7 @@
         "contacts",
         "af_security",
         "hr_skill",
-        'hr',
+        'hr_office',
     ],
     "external_dependencies": [
     ],
@@ -25,7 +25,7 @@
         "data/mail_template.xml",
         "data/calendar_schedule_cron.xml",
         "security/ir.model.access.csv",
-        #"views/hr_operation_view.xml",
+        "views/hr_operation_view.xml",
         "views/res_users_view.xml",
         "views/calendar_af_view.xml",
         "views/calendar_schedule_view.xml",
--- calendar_af_360/__manifest__.py
+++ calendar_af_360/__manifest__.py
@@ -16,7 +16,6 @@
     "data": [
         "views/calendar_occasion_view.xml",
         "views/res_partner_view.xml",
-        "views/hr_operation_view.xml",
         "views/hr_employee_view.xml",
         "views/calendar_occasion_view.xml",
         "views/calendar_appointment_view.xml",
