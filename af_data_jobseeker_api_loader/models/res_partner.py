af32d740118dc4c9ff9ca902ee59fd5c29bdbada
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-12-01 08:49:31 +0100

    af_data_jobseeker_api_loader AFC-1433: changed search function to search_model to not conflict with existing search function

--- af_data_jobseeker_api_loader/models/res_partner.py
+++ af_data_jobseeker_api_loader/models/res_partner.py
@@ -43,13 +43,13 @@ class ResPartner(models.Model):
     @api.model
     def create_partners_from_api(self, key, path):
         db_con = self.env.ref('af_ipf.ipf_endpoint_rask').sudo()
-        db_values = {'res.country.state': self.search('res.country.state', 'code', 'id'),
-                     'hr.departement': self.search('hr.department', 'office_code', 'id'),
-                     'res.sun': self.search('res.sun', 'code', 'id'),
-                     'res.partner.skat': self.search('res.partner.skat', 'code', 'id'),
-                     'res.partner.education_level': self.search('res.partner.education_level', 'name', 'id'),
-                     'res.users': self.search('res.users', 'login', 'id'),
-                     'res.country': self.search('res.country', 'name', 'id', 'lang="sv_SE"'),}
+        db_values = {'res.country.state': self.search_model('res.country.state', 'code', 'id'),
+                     'hr.departement': self.search_model('hr.department', 'office_code', 'id'),
+                     'res.sun': self.search_model('res.sun', 'code', 'id'),
+                     'res.partner.skat': self.search_model('res.partner.skat', 'code', 'id'),
+                     'res.partner.education_level': self.search_model('res.partner.education_level', 'name', 'id'),
+                     'res.users': self.search_model('res.users', 'login', 'id'),
+                     'res.country': self.search_model('res.country', 'name', 'id', 'lang="sv_SE"'),}
         index = 0
         iterations = 0
         with open(path) as fh:
@@ -76,7 +76,7 @@ class ResPartner(models.Model):
                     self.env.cr.commit()
                     iterations = 0
 
-    def search(self, obj, key, field_name, context=None):
+    def search_model(self, obj, key, field_name, context=None):
         """Build dicts with key: field_name"""
         if context:
             return {res[key]: res[field_name] for res in
