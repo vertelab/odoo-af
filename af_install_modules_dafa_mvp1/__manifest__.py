5aa583734fe83ec6a9781156b1c3d7c5db31f057
Artur Chilangwa <artur.chilangwa@vertel.se>
2020-12-02 15:38:40 +0100

    af_install_modules_dafa_mvp(1+2): Format manifests

--- af_install_modules_dafa_mvp1/__manifest__.py
+++ af_install_modules_dafa_mvp1/__manifest__.py
@@ -1,10 +1,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-	"name": "Af Dava MVP 1.0 Install all modules ",
-	"version": "12.0.1.1",
-	"author": "Vertel AB",
-	"description": """
+    "name": "Af Dava MVP 1.0 Install all modules ",
+    "version": "12.0.1.1",
+    "author": "Vertel AB",
+    "description": """
 	This module installs all Dafa MVP1-modules at one go.\n
 	\n
 	Please see the depend-tab (with debug-mode) for a list of which modules are installed.\n
@@ -13,18 +13,18 @@
 	Once the module is installed, please de-install it to avoid depencency-problems.\n
 	\n
 	""",
-	"license": "AGPL-3",
-	"website": "https://vertel.se/",
-	"category": "Tools",
-	"depends": [
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": [
 		# These modules needs to be installed for  MVP2 functionality
-		# 
+		#
 		"auth_saml_ol_groups",
 		"auth_saml_af",
 		"audit_logger",
 		"audit_timeout",
 		"res_drivers_license",
-		"partner_view_360", 
+		"partner_view_360",
 		"contact_links",
 		"partner_mq_ipf",
 		"edi_af_aisf_rask_get_jobseeker",
@@ -41,7 +41,7 @@
 		"web_autocomplete_off",
 		"web_backend_theme_af",
 		"web_dashboard_fenix",
-	],
-	"application": False,
-	"installable": True,
+    ],
+    "application": False,
+    "installable": True,
 }
--- af_install_modules_dafa_mvp2/__manifest__.py
+++ af_install_modules_dafa_mvp2/__manifest__.py
@@ -2,9 +2,9 @@
 
 {
 	"name": "Af Dava MVP 2.0 Install all modules ",
-	"version": "12.0.1.1",
-	"author": "Vertel AB",
-	"description": """
+    "version": "12.0.1.1",
+    "author": "Vertel AB",
+    "description": """
 	This module installs all Dafa MVP2-modules at one go.\n
 	\n
 	Please see the depend-tab (with debug-mode) for a list of which modules are installed.\n
@@ -13,21 +13,21 @@
 	Once the module is installed, please de-install it to avoid depencency-problems.\n
 	\n
 	""",
-    
-	"license": "AGPL-3",
-	"website": "https://vertel.se/",
-	"category": "Tools",
-	"depends": [
+
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": [
 		# These modules needs to be installed for  MVP2 functionality
 		#
-		#"af_sale_orders",
-		#"af_sales_report",
+		# "af_sale_orders",
+		# "af_sales_report",
 		"hr_af_holidays",
-		#"send_mail_nadim",
-		#"mail_oe_chatter_user_groups",
-		#"project_jobseeker_views",
-		#"api_project_odoo_xmlrpc",
-	],
-	"application": False,
-	"installable": True,
+		# "send_mail_nadim",
+		# "mail_oe_chatter_user_groups",
+		# "project_jobseeker_views",
+		# "api_project_odoo_xmlrpc",
+    ],
+    "application": False,
+    "installable": True,
 }
