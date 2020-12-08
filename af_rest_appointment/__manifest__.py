118b0368129262ce31ae01148b008ad3254335fe
Fredrik Arvas <fredrik@arvas.se>
2020-12-08 19:40:38 +0100

    added depreciated as prefix

--- af_rest_appointment/__manifest__.py
+++ af_rest_appointment/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "AF REST Appointment - DEPRECIATED",
+    "name": "Depreciated AF REST Appointment",
     "version": "12.0.1.0.0",
     "author": "Vertel AB",
     "license": "AGPL-3",
