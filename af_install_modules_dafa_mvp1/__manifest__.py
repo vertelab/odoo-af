491d8d1bf705f3e8fffdb6f5d5029bf412d2dd82
Fredrik Arvas <fredrik@arvas.se>
2020-12-26 07:58:06 +0100

    Syncronized Repo-list

--- af_install_modules_dafa_mvp1/__manifest__.py
+++ af_install_modules_dafa_mvp1/__manifest__.py
@@ -19,7 +19,46 @@
 	v12.0.1.8  - Disableded Matomo due to bug. Disabled hr_org_chart since Emma did not want it.\n
 	Once the module is installed, please de-install it to avoid depencency-problems.\n
 	v12.0.1.0.9  - Added new version-numbers-standard. Added hr_af_holidays.\n	
+	v12.0.1.0.10  - Added list of Repos that needs to be installed.\n	
 	\n
+	\n	
+	\n
+
+# Vertel
+"odoo-account||https://github.com/vertelab/odoo-account.git" 
+"odoo-af||https://github.com/vertelab/odoo-af.git"
+"odoo-api||https://github.com/vertelab/odoo-api.git" 
+"odoo-auth||https://github.com/vertelab/odoo-auth.git" # New branch Dev-12.0-Fenix-Sprint-02 based on Dev-12.0
+"odoo-base||https://github.com/vertelab/odoo-base.git"
+# "odoo-contract|master|https://github.com/vertelab/odoo-contract.git"
+"odoo-edi||https://github.com/vertelab/odoo-edi.git" # New branch Dev-12.0-Fenix-Sprint-02 based on Dev-12.0
+"odoo-hr||https://github.com/vertelab/odoo-hr.git"
+# "odoo-imagemagick||https://github.com/vertelab/odoo-imagemagick.git" # Not used in DAFA
+"odoo-l10n_se|12.0|https://github.com/vertelab/odoo-l10n_se.git" # Need to explicitly say 12.0 else it will use the default branch Dev-12.0-Fenix-Sprint-02. We want 12.0 for the moment
+"odoo-mail||https://github.com/vertelab/odoo-mail.git"
+"odoo-outplacement||https://github.com/vertelab/odoo-outplacement.git"
+"odoo-project||https://github.com/vertelab/odoo-project.git"
+"odoo-sale||https://github.com/vertelab/odoo-sale.git"
+"odoo-server-tools||https://github.com/vertelab/odoo-server-tools.git" # New branch Dev-12.0-Fenix-Sprint-02 based on Dev-12.0
+"odoo-user-mail||https://github.com/vertelab/odoo-user-mail.git"
+"odoo-web||https://github.com/vertelab/odoo-web.git" # New branch Dev-12.0-Fenix-Sprint-02 based on Dev-12.0
+# "odoo-website||https://github.com/vertelab/odoo-website.git"
+
+# Use Vertel versions of OCA modules
+"odooext-oca-web||https://github.com/vertelab/web.git"
+"odooext-oca-server-auth||https://github.com/vertelab/server-auth.git" # New branch Dev-12.0-Fenix-Sprint-02 based on Dev-12.0
+
+# OCA
+"odooext-oca-contract||https://github.com/OCA/contract.git" 
+"odooext-oca-hr||https://github.com/OCA/hr.git"
+"odooext-oca-mis-builder||https://github.com/OCA/mis-builder.git"
+"odooext-oca-partner-contact||https://github.com/OCA/partner-contact.git"
+"odooext-oca-reporting-engine||https://github.com/OCA/reporting-engine.git"
+"odooext-oca-server-ux||https://github.com/OCA/server-ux.git"
+"odooext-oca-social||https://github.com/OCA/social.git"
+"odooext-oca-timesheet||https://github.com/OCA/timesheet.git"
+	
+	
 	""",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
