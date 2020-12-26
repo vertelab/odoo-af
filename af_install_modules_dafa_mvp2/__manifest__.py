68c4e1db4b6e0d5d3313db6123aed72c439e4598
Fredrik Arvas <fredrik@arvas.se>
2020-12-26 06:58:40 +0100

    Added a complete list of dependecies to modules

--- af_install_modules_dafa_mvp2/__manifest__.py
+++ af_install_modules_dafa_mvp2/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "DAFA MVP 2.0 Install all modules ",
-    "version": "12.0.1.0.7",
+    "version": "12.0.1.1.0",
     "category": 'Outplacement',
     "author": "Vertel AB",
     "description": """
@@ -17,11 +17,12 @@
         v12.0.1.4  - Moved modules about the Jobseeker to MVP2\n
         v12.0.1.5  - Updated modules and projects to install\n
         v12.0.1.6  - Added generic CoA\n    
-        v12.0.1.0.7  - Added category Outplacement\n
+        v12.0.1.0.7  - Added category Outplacement and new number standard (Odoo version 12.0 and Major, Minor, Patch)\n
+        v12.0.1.1.0  - Added a complete list of dependencies to modules\n
     
         Branch Dev-12.0-Fenix-Sprint-02 fallback 12.0
         
-        Vertel-projekt att installera 
+        Vertel-project to install 
 
         odoo-account
         odoo-af
@@ -38,7 +39,7 @@
         odoo-server-tools
         odoo-web
 
-        OCA-project att installera
+        OCA-projects to install
 
         odooext-OCA-contract
         odooext-OCA-hr
@@ -52,39 +53,29 @@
         odooext-OCA-web
 
 
-        Moduler att installera:
-        l10n_generic_coa
-        hr_timesheet
-        outplacement
-        #outplacement_deviation_report
-        outplacement_joint_planning
-        outplacement_order_interpretor
-        outplacement_partner_education
-        outplacement_partner_jobs
-        outplacement_partner_ssn
-        outplacement_invoice
-        sale_management
-        sale_outplacement
-
     """,
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     "category": "Tools",
     "depends": [
     # Modules for DAFA MVP2:
-        "l10n_generic_coa",                 # adds a generic chart of accounts and solves an error in sale_management
-        "outplacement",
-        #outplacement_deviation_report      # dependancy to outplacment, 
-        "outplacement_joint_planning",      # dependancy to outplacment, 
-        "outplacement_order_interpretor",   # dependancy to outplacment, mail
-        "outplacement_partner_education",   # dependancy to outplacment, res_sun, res_partner_drivers_licenses
-        "outplacement_partner_jobs",        # dependancy to outplacment, res_ssyk
-        "outplacement_partner_skills",
-        "outplacement_partner_ssn",
-        "outplacement_invoice",
-        "sale_management",
-        "sale_outplacement",                # dependancy to sale_managment
-        "hr_timesheet",
+        "hr_skill",                         # dependancy to hr
+        "hr_timesheet",                     # dependancy to hr, analytic, project, uom
+        "outplacement",                     # dependancy to base, hr, mail 
+        "outplacement_completion_report_ipf_client",    # dependancy to res_joint_planning_af
+        #"outplacement_deviation_report     # dependancy to outplacment, outplacement_deviationreport_ipf_client (which is not ready yet)
+        "outplacement_joint_planning",      # dependancy to outplacment, res_joint_planning_af, outplacement_completion_report_ipf_client, project
+        "outplacement_order_interpretor",   # dependancy to outplacment, mail, hr_timesheet, project, task_interpretor_ipf_client
+        "outplacement_partner_education",   # dependancy to outplacment, partner_education_views
+        "outplacement_partner_jobs",        # dependancy to outplacment, partner_desired_jobs
+        "outplacement_partner_skills",      # dependancy to outplacment, hr_skill
+        "outplacement_partner_ssn",         # dependency to outplacment, partner_ssn 
+        "outplacement_invoice",             # dependency to outplacment, account
+        "partner_desired_jobs",             # depenedncy to base, hr_skill, res_ssyk, res_sun
+        "partner_education_views",          # dependency to contacts
+        "res_joint_planning_af"             # dependency to outplacment,
+        "sale_outplacement",                # dependancy to outplacement, sale, (sale_managment), res_joint_planning_af, sale_suborder_ipf_server, l10n_se, project
+        "sale_suborder_ipf_server",         # dependancy to outplacement, web
 
     ],
     "application": "False",
