e92d18ae9cec0fff386b681209292d45f9cd3d69
Fredrik Arvas <fredrik@arvas.se>
2020-12-26 07:12:00 +0100

    added xml_rpc

--- af_install_modules_dafa_mvp2/__manifest__.py
+++ af_install_modules_dafa_mvp2/__manifest__.py
@@ -10,12 +10,11 @@
     "website": "https://vertel.se/",
 
     "description": """
-        This module installs all Dafa MVP2-modules at one go.\n
+        This module adds modules on top of the DAFA MVP1 modueles. It installs all DAFA MVP2-modules at one go.\n
         \n
         Please see the depend-tab (with debug-mode) for a list of which modules are installed.\n
         \n
-        v12.0.1.1  - First release\n
-        Once the module is installed, please de-install it to avoid depencency-problems.\n
+        v12.0.1.1  - First release. Once the module is installed, please de-install it to avoid upgrade-problems.\n
         v12.0.1.2  - Uncommented xmlrpc\n
         v12.0.1.3  - Fixed name on api_odoo_xmlrpc\n
         v12.0.1.4  - Moved modules about the Jobseeker to MVP2\n
@@ -58,21 +57,22 @@
     """,
     "depends": [
     # Modules for DAFA MVP2:
+        "api_odoo_xmlrpc"                   # dependency to base_setup (adds sync to an other Odoo-server)
         "hr_skill",                         # dependancy to hr
         "hr_timesheet",                     # dependancy to hr, analytic, project, uom
         "outplacement",                     # dependancy to base, hr, mail 
         "outplacement_completion_report_ipf_client",    # dependancy to res_joint_planning_af
-        #"outplacement_deviation_report     # dependancy to outplacment, outplacement_deviationreport_ipf_client (which is not ready yet)
-        "outplacement_joint_planning",      # dependancy to outplacment, res_joint_planning_af, outplacement_completion_report_ipf_client, project
-        "outplacement_order_interpretor",   # dependancy to outplacment, mail, hr_timesheet, project, task_interpretor_ipf_client
-        "outplacement_partner_education",   # dependancy to outplacment, partner_education_views
-        "outplacement_partner_jobs",        # dependancy to outplacment, partner_desired_jobs
-        "outplacement_partner_skills",      # dependancy to outplacment, hr_skill
-        "outplacement_partner_ssn",         # dependency to outplacment, partner_ssn 
-        "outplacement_invoice",             # dependency to outplacment, account
+        #"outplacement_deviation_report     # dependancy to outplacement, outplacement_deviationreport_ipf_client (which is not ready yet)
+        "outplacement_joint_planning",      # dependancy to outplacement, res_joint_planning_af, outplacement_completion_report_ipf_client, project
+        "outplacement_order_interpretor",   # dependancy to outplacement, mail, hr_timesheet, project, task_interpretor_ipf_client
+        "outplacement_partner_education",   # dependancy to outplacement, partner_education_views
+        "outplacement_partner_jobs",        # dependancy to outplacement, partner_desired_jobs
+        "outplacement_partner_skills",      # dependancy to outplacement, hr_skill
+        "outplacement_partner_ssn",         # dependency to outplacement, partner_ssn 
+        "outplacement_invoice",             # dependency to outplacement, account
         "partner_desired_jobs",             # depenedncy to base, hr_skill, res_ssyk, res_sun
         "partner_education_views",          # dependency to contacts
-        "res_joint_planning_af"             # dependency to outplacment,
+        "res_joint_planning_af"             # dependency to outplacement,
         "sale_outplacement",                # dependancy to outplacement, sale, (sale_managment), res_joint_planning_af, sale_suborder_ipf_server, l10n_se, project
         "sale_suborder_ipf_server",         # dependancy to outplacement, web
 
