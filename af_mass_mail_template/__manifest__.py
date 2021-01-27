72e864b8efc4748dce8bd6377e432cdbf10262a0
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-01-27 14:01:13 +0100

    Update __manifest__.py

--- af_mass_mail_template/__manifest__.py
+++ af_mass_mail_template/__manifest__.py
@@ -6,6 +6,10 @@
 	"description": """
 	 v12.0.0.1 AFC-86  Add Mass mailing template for AF newsletters.\n
 	 check if this module is worth having. mail_tracking_mass_mailing\n
+	 check if this module is worth having. mass_mailing_custom_unsubscribe\n
+	 check if this module is worth having. mass_mailing_resend \n
+	 check if this module is worth having. partner_email_check \n
+	 check if this module is worth having. test_mass_mailing \n
     """,
     "license": "AGPL-3",
     "author": "Vertel AB",
