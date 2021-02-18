3438eb955ab4b04788a91563155dc4bf2ae45548
Daniel Eriksson <daniel.eriksson@arbetsformedlingen.se>
2021-02-18 08:03:49 +0100

    af_install_modules_dafa_mvp1: removed wrong SAML-module

--- af_install_modules_dafa_mvp1/__manifest__.py
+++ af_install_modules_dafa_mvp1/__manifest__.py
@@ -5,24 +5,24 @@
     "version": "12.0.1.0.11",
     "author": "Vertel AB",
     "description": """
-	This module installs all Dafa MVP1-modules at one go.\n
-	\n
-	Please see the depend-tab (with debug-mode) for a list of which modules are installed.\n
-	\n
-	v12.0.1.1  - First release\n
-	v12.0.1.2  - Updates in the list\n
-	v12.0.1.3  - Added Auth_user_rights_wizard and CIAM-modules, corrected spelling of auth_timeout\n
-	v12.0.1.4  - Removed items outside of MVP1 \n
-	v12.0.1.5  - Added more modules \n
-	v12.0.1.6  - Removed auth_login \n
-	v12.0.1.7  - Added my customer_tab and hr_holidays \n
-	v12.0.1.8  - Disableded Matomo due to bug. Disabled hr_org_chart since Emma did not want it.\n
-	Once the module is installed, please de-install it to avoid depencency-problems.\n
-	v12.0.1.0.9  - Added new version-numbers-standard. Added hr_af_holidays.\n	
-	v12.0.1.0.10  - Added list of Repos that needs to be installed.\n	
-	v12.0.1.0.11  - AFC-1666 added partner_flip_firstname.\n	
-	\n	
-	\n
+    This module installs all Dafa MVP1-modules at one go.\n
+    \n
+    Please see the depend-tab (with debug-mode) for a list of which modules are installed.\n
+    \n
+    v12.0.1.1  - First release\n
+    v12.0.1.2  - Updates in the list\n
+    v12.0.1.3  - Added Auth_user_rights_wizard and CIAM-modules, corrected spelling of auth_timeout\n
+    v12.0.1.4  - Removed items outside of MVP1 \n
+    v12.0.1.5  - Added more modules \n
+    v12.0.1.6  - Removed auth_login \n
+    v12.0.1.7  - Added my customer_tab and hr_holidays \n
+    v12.0.1.8  - Disableded Matomo due to bug. Disabled hr_org_chart since Emma did not want it.\n
+    Once the module is installed, please de-install it to avoid depencency-problems.\n
+    v12.0.1.0.9  - Added new version-numbers-standard. Added hr_af_holidays.\n	
+    v12.0.1.0.10  - Added list of Repos that needs to be installed.\n	
+    v12.0.1.0.11  - AFC-1666 added partner_flip_firstname.\n	
+    \n	
+    \n
 
 # Vertel
 "odoo-account||https://github.com/vertelab/odoo-account.git" 
@@ -57,48 +57,47 @@
 "odooext-oca-server-ux||https://github.com/OCA/server-ux.git"
 "odooext-oca-social||https://github.com/OCA/social.git"
 "odooext-oca-timesheet||https://github.com/OCA/timesheet.git"
-	
-	
-	""",
+""",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     "category": "Tools",
     "depends": [
-		# These modules needs to be installed for  MVP1 functionality
-		# "af_statistics",  # will be renamed to web_matomo. Disabled since there is a bug - the menu does not render every time.
-		"api_ipf",  # Configuration of API:s that goes towards IPF.
-		"api_ipf_tlr_client",  # adds api to be used for fetching company-details from TLR
-		"auth_saml_ol_groups",
-		"auth_saml_af",
-		# "auth_session_timeout", # Adds ability to set session inactive timeout as system parameter. There is a conflict with: levavrop
-		# adds functionality to assign user-credentials to coaches.
-		"auth_user_rights_wizard",
-		"audit_logger",
-		# "auth_timeout",  # Disabled due to conflict with auth_saml_ol AFC-1547
-		"hr_departments_partner",
-		"hr_af_holidays", # Adds non-working-days for Af. Should be name-changed to hr_holidays_af
-		"hr_user_ciam_update",  # adds users to the CIAM-server
-		"hr_employee_firstname",  # is required by hr_employee_firstname_extension
-		"hr_employee_firstname_extension",  # is dependent on hr_employee_firstname
-		"hr_employee_legacy_id",  # OCA
-		# "hr_employee_views_fenix", # Depricated, renamed to hr_employee_views_dafa
-		# "hr_employee_views_dafa",
-		# "hr_employee_views_user-credentials-tab", - depreciated. Replaced by user-create-wizard.
-		# "hr_af_holidays", # dependant on hr_holidays
-		"hr_office",  # Adds
-		# "hr_org_chart", #Disabled since Emma did not want it.
-		# dependant on: partner_legacy_id, partner_firstname, api_ipf_tlr_client, hr_departments_partner
-		"partner_flip_firstname",	# Flips first-name and lastname in contact-view.
-		"partner_tlr_update",
-		# "web_a11y_filter_view", # adds description to create and edit buttons. Disabled since there is a bug
-		"web_a11y_report",      # adds a report with status of the Accessibility-status
-		# adds a field used to store the TLR lev_id and the employees id in other salary-systems.
-		"partner_legacy_id",
-		# "web_autocomplete_off",
-		"web_backend_theme_af",
-		"web_dashboard_dafa",
-		# Adds a ribbon with db- and version info in the left corner.
-		"web_environment_ribbon",
+        # These modules needs to be installed for  MVP1 functionality
+        # "af_statistics",  # will be renamed to web_matomo. Disabled since there is a bug - the menu does not render every time.
+        "api_ipf",  # Configuration of API:s that goes towards IPF.
+        "api_ipf_tlr_client",  # adds api to be used for fetching company-details from TLR
+        "auth_saml_ol_groups",
+        "auth_saml_dafa",
+        # "auth_saml_af", # wrong saml module for dafa project
+        # "auth_session_timeout", # Adds ability to set session inactive timeout as system parameter. There is a conflict with: levavrop
+        # adds functionality to assign user-credentials to coaches.
+        "auth_user_rights_wizard",
+        "audit_logger",
+        # "auth_timeout",  # Disabled due to conflict with auth_saml_ol AFC-1547
+        "hr_departments_partner",
+        "hr_af_holidays",  # Adds non-working-days for Af. Should be name-changed to hr_holidays_af
+        "hr_user_ciam_update",  # adds users to the CIAM-server
+        "hr_employee_firstname",  # is required by hr_employee_firstname_extension
+        "hr_employee_firstname_extension",  # is dependent on hr_employee_firstname
+        "hr_employee_legacy_id",  # OCA
+        # "hr_employee_views_fenix", # Depricated, renamed to hr_employee_views_dafa
+        # "hr_employee_views_dafa",
+        # "hr_employee_views_user-credentials-tab", - depreciated. Replaced by user-create-wizard.
+        # "hr_af_holidays", # dependant on hr_holidays
+        "hr_office",  # Adds
+        # "hr_org_chart", #Disabled since Emma did not want it.
+        # dependant on: partner_legacy_id, partner_firstname, api_ipf_tlr_client, hr_departments_partner
+        "partner_flip_firstname",  # Flips first-name and lastname in contact-view.
+        "partner_tlr_update",
+        # "web_a11y_filter_view", # adds description to create and edit buttons. Disabled since there is a bug
+        "web_a11y_report",  # adds a report with status of the Accessibility-status
+        # adds a field used to store the TLR lev_id and the employees id in other salary-systems.
+        "partner_legacy_id",
+        # "web_autocomplete_off",
+        "web_backend_theme_af",
+        "web_dashboard_dafa",
+        # Adds a ribbon with db- and version info in the left corner.
+        "web_environment_ribbon",
     ],
     "application": "False",
     "installable": "True",
