6a6296d7b3f884107b62f3cc79fffcb0a0048a95
Fredrik Arvas <fredrik@arvas.se>
2021-02-22 16:15:14 +0100

    Fler tillägg

--- af_install_modules_dafa_mvp1/__manifest__.py
+++ af_install_modules_dafa_mvp1/__manifest__.py
@@ -64,39 +64,50 @@
     "depends": [
         # These modules needs to be installed for  MVP1 functionality
         "account_period",  # Added period for accounting
+        "af_security",      # Security-rules for AF
         # "af_statistics",  # will be renamed to web_matomo. Disabled since there is a bug - the menu does not render every time.
         "api_ipf",  # Configuration of API:s that goes towards IPF.
         "api_ipf_tlr_client",  # adds api to be used for fetching company-details from TLR
-        "auth_saml_ol_groups",
+        "audit_logger",     # Logging module
+        "auth_admin",
         "auth_saml_dafa",
+        "auth_saml_ol_",
+        "auth_saml_ol_create_user",
+        "auth_saml_ol_groups",
+        "auth_saml_fix",
         # "auth_session_timeout", # Adds ability to set session inactive timeout as system parameter. There is a conflict with: levavrop
-        "auth_user_rights_wizard",
-        "audit_logger",
         # "auth_timeout",  # Disabled due to conflict with auth_saml_ol AFC-1547
-        "hr_departments_partner",
+        "auth_user_rights_wizard",
+        "base_user_groups_dafa",
         "hr_af_holidays",  # Adds non-working-days for Af. Should be name-changed to hr_holidays_af
-        "hr_user_ciam_update",  # adds users to the CIAM-server
-        "hr_emploee_ciam_client", # CIAM Client for AF project, API module to handle communication between DAFA (formerly called Fenix) and CIAM
+        "hr_departments_partner",
+        "hr_employee_ciam_client", # CIAM Client for AF project, API module to handle communication between DAFA (formerly called Fenix) and CIAM
         "hr_employee_firstname",  # is required by hr_employee_firstname_extension
         "hr_employee_firstname_extension",  # is dependent on hr_employee_firstname
         "hr_employee_legacy_id",  # OCA
-        "hr_employee_views_dafa", # Mostly hides various fields and pages in the hr.employee form
+        "hr_employee_ssn",
         "hr_holidays", # Odoo module for leaves management 
         "hr_office",  # Adds
         # "hr_org_chart", # Disabled since Emma did not want it.
         "hr_outplacement_tab",              # dependancy to hr, outplacment
         "hr_skill",                         # dependancy to hr
+        "hr_user_ciam_update",  # adds users to the CIAM-server
         "hr_timesheet",                     # dependancy to hr, analytic, project, uom
+        "l10n_se",
+        "l10n_se_mis",
+        "mail_outplacement_report",
+        "mis_builder",
         "outplacement",                     # dependancy to base, hr, mail
         "outplacement_completion_report_ipf_client",    # dependancy to res_joint_planning_af
         # "outplacement_crmsync",           # Module to be installed at a DAFA server and communicate with outplacement_dafasync" 
         "outplacement_dafa",                # 
         "outplacement_deviation_report",  # dependancy to outplacement, outplacement_deviationreport_ipf_client (which is not ready yet)
         "outplacement_deviation_report_ipf_client",  # dependancy to outplacement, outplacement_deviationreport_ipf_client (which is not ready yet)
-        # "outplacement_final_report",      # Not ready
-        # "outplacement_final_report_send", # Not ready
-        # "outplacement_final_report_ipf_client", # Not ready
+        "outplacement_final_report",      # 
+        "outplacement_final_report_ipf_client", # 
+        "outplacement_final_report_send", # 
         "outplacement_joint_planning",      # dependancy to outplacement, res_joint_planning_af, outplacement_completion_report_ipf_client, project
+        "outplacement_order",               # dependancy to outplacement, mail, hr_timesheet, project, task_interpreter_ipf_client
         "outplacement_order_interpreter",   # dependancy to outplacement, mail, hr_timesheet, project, task_interpreter_ipf_client
         "outplacement_partner_af_signature", # 
         "outplacement_partner_education",   # dependancy to outplacement, partner_education
@@ -106,25 +117,31 @@
         "partner_af_signature",             # 
         "partner_desired_jobs",             # depenedncy to base, hr_skill, res_ssyk, res_sun
         "partner_education",                # dependency to contacts
+        "partner_firstname",           # Flips first-name and lastname in contact-view.
         "partner_flip_firstname",           # Flips first-name and lastname in contact-view.
         "partner_legacy_id",                # adds a field used to store the TLR lev_id and the employees id in other salary-systems.
+        "partner_ssn",
         "partner_tlr_update",
-        # "web_a11y_filter_view",           # adds description to create and edit buttons. 
-        "web_a11y_report",                  # adds a report with status of the Accessibility-status
+        "res_drivers_license",
         "res_interpreter_gender_preference", # Data repository for interpreter gender preference codes.
         "res_interpreter_language",         # Data repository for interpreter language codes
         "res_interpreter_remote_type",      # Data repository for interpreter type codes.
+        "res_interpreter_type",
         "res_joint_planning_af",            # dependency to outplacement,
-        "res_drivers_license",
         "res_ssyk",
         "res_sun",
-        "sale_outplacement",                # dependancy to outplacement, sale, (sale_managment), res_joint_planning_af, sale_suborder_ipf_server, l10n_se, project
-        "sale_suborder_ipf_server",         # dependancy to outplacement, web
         "sale_management",                  # Odoo Module to display Sales
+        "sale_outplacement",                # dependancy to outplacement, sale, (sale_managment), res_joint_planning_af, sale_suborder_ipf_server, l10n_se, project
+        "sale_showorder_ipf_client",
+        "sale_suborder_ipf_server",         # dependancy to outplacement, web
         "task_interpreter_ipf_client",      # IntepreatorBookings integration for REST-calls from the client-module to the server-module.
+        "web_a11y_create-buttons",           # adds description to create and edit buttons. 
+        "web_a11y_report",                  # adds a report with status of the Accessibility-status
         "web_backend_theme_af",
         "web_dashboard_dafa",
         # "web_environment_ribbon",           # Adds a ribbon with db- and version info in the left corner.
+        "web_one2many_kanban",
+        "web_widget_color",
         
     ],
     "application": "False",
