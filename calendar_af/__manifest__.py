e07aea449efea353b52d7b6f94808b4a101e5802
Daniel Eriksson <47656513+daerio@users.noreply.github.com>
2021-03-25 10:46:53 +0100

    Update __manifest__.py

--- calendar_af/__manifest__.py
+++ calendar_af/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Calendar management - AF",
-    "version": "12.0.1.0.7",
+    "version": "12.0.1.0.8",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
