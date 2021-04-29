8751bfbf35223cbaf160b43f08277fc5a866be66
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-04-29 08:28:22 +0200

    Update __manifest__.py

--- af_statistics/__manifest__.py
+++ af_statistics/__manifest__.py
@@ -4,9 +4,17 @@
 {
     "name": "AF Statistics",
     "summary": "AF Statistics for Matomo",
-    "version": "12.0.0.2",
+    "version": "12.0.1.0.3",
     "category": "TBD",
-    "description": "AF Statistics for Matomo",
+    "description": """AF Statistics for Matomo
+    
+// CRM PROD https://af.analytics.elx.cloud/js/container_4v00kfpD.js
+// Test CRM: https://af-staging.analytics.elx.cloud/js/container_ew8X7e8B.js
+// DAFA PROD https://af.analytics.elx.cloud/js/container _t72Scj0c.js
+// Test DAFA: https://af-staging.analytics.elx.cloud/js/container_n7teu7kr.js
+
+
+""",
     "author": "AF",
     "license": "AGPL-3",
     "installable": True,
