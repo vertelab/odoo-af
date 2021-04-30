c28304fad80cc764a02b05762c7dc20f4abc0df9
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-04-30 07:42:15 +0200

    Depreciated and replaced by web_statistics_af

--- af_statistics/__manifest__.py
+++ af_statistics/__manifest__.py
@@ -2,8 +2,8 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "AF Statistics",
-    "summary": "AF Statistics for Matomo",
+    "name": "Depreciated AF Statistics",
+    "summary": "Replaced by web_statistics_af",
     "version": "12.0.0.2",
     "category": "TBD",
     "description": "AF Statistics for Matomo",
