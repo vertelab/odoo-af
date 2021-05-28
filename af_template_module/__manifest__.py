6434f0a2e29a3a545bdced866d86406ae86a0ff0
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-05-28 14:34:38 +0200

    Update __manifest__.py

--- af_template_module/__manifest__.py
+++ af_template_module/__manifest__.py
@@ -2,12 +2,14 @@
     "name": "Af Template Module",
     "version": "12.0.1.0",
     "author": "Vertel AB",
+    "maintainer": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     "category": "Tools",
     'description': """
 Test Module \n
 ======================================================\n
+This module is maintained from: https://github.com/vertelab/odoo-af/edit/Dev-12.0/af_template_module/ 
 Hover over fields to se a brief description of them \n
 For more information make sure you are in debug mode \n
 v12.0.1.0 - AFC-123 Added the module to the repo \n
