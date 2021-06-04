dfeb9bc01be194023f7832a3c299cbd00d385537
Daniel Eriksson <daer@daer.io>
2021-06-04 11:10:34 +0200

    af_template_module AFC-112: gjort något

--- af_template_module/__manifest__.py
+++ af_template_module/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Af Template Module",
-    "version": "12.0.1.2.4",
+    "version": "12.0.1.2.5",
     "author": "Vertel AB",
     "maintainer": "Vertel AB",
     "license": "AGPL-3",
@@ -18,6 +18,7 @@ Hover over fields to se a brief description of them \n
 For more information make sure you are in debug mode \n
 v12.0.1.0 - AFC-123 Added the module to the repo \n
 v12.0.1.2.4 - AFC-999 någon ändring \n
+v12.0.1.2.5 - AFC-112: daniels ändring
 
 """,
     "depends": [
