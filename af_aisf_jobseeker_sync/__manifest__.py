c960463fcde590ab5ffd82348736637890a22750
aldindrag <draal@89-21.wt.ams.se>
2021-06-24 09:01:54 +0200

    AFC-2263 - Changed boolean to selection field

--- af_aisf_jobseeker_sync/__manifest__.py
+++ af_aisf_jobseeker_sync/__manifest__.py
@@ -2,13 +2,14 @@
 
 {
     'name': 'AIS-F Jobseeker Sync',
-    'version': '12.0.0.2.1',
+    'version': '12.0.0.2.2',
     'summary': 'Sync Jobseeker data from AIS-F.',
     'description': """AIS-F sync
 ================================================================================================
 v12.0.0.1.0: AIS-F sync implemented.\n
 v12.0.0.2.0 AFC-2245: Added better handling of jobseekers with SPU. \n
 v12.0.0.2.1 AFC-2254: Misc changes \n
+v12.0.0.2.2 AFC-2263: Changed boolean to selection field \n
 """,
     'category': 'Technical Settings',
     'author': 'Vertel AB',
