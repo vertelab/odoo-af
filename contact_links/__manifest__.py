bc1260f810e420c8cad4364f83ebf4515c1025af
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-07-12 09:21:25 +0200

    Updated module description for test environment

--- contact_links/__manifest__.py
+++ contact_links/__manifest__.py
@@ -7,11 +7,15 @@
     'description': """
 Cases
 ===============================================================================
-This module adds Links to a partner.
-Added server action 'Update Link' to add all links in contact.Select contacts in which you want to add links and click on
-Actions -> Update Link. If you can't see 'Update Link' inside Action then Go to Setting >> Technical >> Actions >> Server
-Actions and search 'Update Link'. Open it and click on 'Create Contexual Action' button then refresh browser.
-
+This module adds Links to for a partner. \n
+Added server action 'Update Link' to add all links in contact.Select contacts in which you want to add links and click on \n
+Actions -> Update Link. If you can't see 'Update Link' inside Action then Go to Setting >> Technical >> Actions >> Server \n
+Actions and search 'Update Link'. Open it and click on 'Create Contexual Action' button then refresh browser. \n
+ \n
+The links in the module works in production.  \n
+For links in test environments see the readme.rst-file for instruction.  \n
+Links that are nor working are prefixed with 'n/a' to illustrate that they are not working yet.  \n
+ \n
 v12.0.0.1.2: Updated manifest and added Af-logo to description. \n
              Updated the security-groups in the datafile to match new version of af_security-groups. \n
 v12.0.0.1.4: Made sure the contact links are computed with the customer number\n
@@ -19,6 +23,7 @@ v12.0.0.1.5: Updated link URLs. \n
 v12.0.0.1.6: Added AIS link. \n
 v12.0.0.2.0 AFC-2229: Added sort order for links. \n
 v12.0.0.2.1 AFC-2254: Updated URL for a few links. \n
+
 """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
