a3825f7219880b689a7737f1fc9e970f8c61146f
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-09-01 10:18:16 +0200

    Update __manifest__.py

--- af_mail_client_employee/__manifest__.py
+++ af_mail_client_employee/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    "name": "AF Mail Client Employee",
+    "name": "Depreciated AF Mail Client Employee",
     "summary": "AF Mail Client Employee",
     "version": "12.0.0.1",
     "category": "Messaging",
 	"description": """
 	 v12.0.0.1 AFC-1313 Mail Client Odoo Employee.
+	 This module is replace by: https://github.com/vertelab/odoo-hr/tree/Dev-12.0/hr_employee_mail_client
     """,
     "license": "AGPL-3",
     "author": "Vertel AB",
