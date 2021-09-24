6f143bbd856e1834aabd80a7a1f69ed8c96c747c
Sebastian Gieser <sebastian.gieser@arbetsformedlingen.se>
2021-09-24 14:45:45 +0200

    af_aisf_jobseeker_sync AFC-2728: updated manifest, added dependency

--- af_aisf_jobseeker_sync/__manifest__.py
+++ af_aisf_jobseeker_sync/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'AIS-F Jobseeker Sync',
-    'version': '12.0.0.2.3',
+    'version': '12.0.0.3.0',
     'summary': 'Sync Jobseeker data from AIS-F.',
     'description': """AIS-F sync
 ================================================================================================
@@ -11,12 +11,13 @@ v12.0.0.2.0 AFC-2245: Added better handling of jobseekers with SPU. \n
 v12.0.0.2.1 AFC-2254: Misc changes \n
 v12.0.0.2.2 AFC-2263: Changed boolean to selection field \n
 v12.0.0.2.3 AFC-2324: Enabled multiple next contact types \n
+v12.0.0.3.0 AFC-2728: Added fields for res.arbetssokande \n
 """,
     'category': 'Technical Settings',
     'author': 'Vertel AB',
     'website': 'https://vertel.se',
     'license': 'AGPL-3',
-    'depends': ['af_process_log', 'af_ipf', 'partner_view_360', 'partner_education'],
+    'depends': ['af_process_log', 'af_ipf', 'res_arbetssokande', 'partner_education'],
     'data': [],
     'demo': [],
     'installable': True,
