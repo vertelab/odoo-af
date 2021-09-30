c5221145f845b3b5dd93fe6566fd5097aa1c8850
Sebastian Gieser <sebastian.gieser@arbetsformedlingen.se>
2021-09-30 16:13:10 +0200

    af_aisf_jobseeker_matching_sync AFC-2534: removed a lot of old copy paste, added outline for data mapping

--- af_aisf_jobseeker_matching_sync/models/res_partner.py
+++ af_aisf_jobseeker_matching_sync/models/res_partner.py
@@ -73,303 +73,79 @@ class Partner(models.Model):
                 info_1=time_for_call_to_mask_matching,
             )
             return
-        customer_id = res.get("arbetssokande", {}).get("sokandeId")
-
-        if res.get("processStatus", {}).get("skyddadePersonUppgifter"):
-            # jobseeker has SPU status. partner should be removed
-            if partner:
-                # cancel all meetings with this jobseeker
-                cancel_reason = self.env.ref(
-                    "calendar_af.reason_authority", raise_if_not_found=False
-                )
-                # if we didn't find the cancel_reason we do not have
-                # calendar_af module installed and there's no need to
-                # continue with canceling meetings. It would crash.
-                if cancel_reason and hasattr(partner, 'appointment_ids'):
-                    partner.appointment_ids._cancel(cancel_reason)
-                # Use special method _try_unlink_class to unlink this partner.
-                # This is done in order to avoid psql locking our cursor if
-                # we run into a problem while unlinking.
-                partner_id = partner.id
-                dbname = self.env.cr.dbname
-                db = odoo.sql_db.db_connect(dbname)
-                cr_unlink = db.cursor()
-                reg_unlink = odoo.registry(dbname)[self._name]
-                res_unlink = reg_unlink._try_unlink_class(
-                    partner_id, cr_unlink, self.env.uid
-                )
-                if not res_unlink:
-                    # partner was unlinked
-                    log.log_message(
-                        process_name,
-                        eventid,
-                        MASK_SYNC,
-                        objectid=customer_id,
-                        error_message="SPU",
-                        info_1=time_for_call_to_mask_matching,
-                        info_2="delete",
-                    )
-                else:
-                    # partner could not be unlinked
-                    log.log_message(
-                        process_name,
-                        eventid,
-                        MASK_SYNC,
-                        objectid=customer_id,
-                        error_message="SPU",
-                        info_1=time_for_call_to_mask_matching,
-                        info_2=res_unlink,
-                    )
-                cr_unlink.close()
-            else:
-                log.log_message(
-                    process_name,
-                    eventid,
-                    MASK_SYNC,
-                    objectid=customer_id,
-                    error_message="SPU",
-                    info_1=time_for_call_to_mask_matchingk,
-                    info_2="not created",
-                )
-            return True
 
         try:
-            state = res.get("kontaktuppgifter", {}).get("hemkommunKod")
-            # TODO: Would probably be best to add country here? I believe
-            #  code is unique per country.
-            if not db_values:
-                state = state and self.env["res.country.state"].search_read(
-                    [("code", "=", state)], ["id"], limit=1
-                )
-                state = state and state[0]["id"] or None
-            else:
-                state = db_values["res.country.state"].get(state, False)
+            {
+                "arbetstid": res.get("arbetstid"),
+                "arbetstid_beskrivning": res.get("arbetstidBeskrivning"),
+                "meriter_finns": res.get("meriterFinns"),
+                "meriter_uppdat_datum": res.get("meriterUppdatDatum"),
+                "flytta_veckopendla": res.get("flyttaVeckopendla"),
+                "soker_inom_eu": res.get("sokerInomEu"),
+                "matchningsbar": res.get("matchningsbar"),
+                "kompl_matchning_info": res.get("komplMatchningInfo"),
+                "korkort": res.get("korkort"),
+                "tillgang_bil": res.get("tillgangBil"),
+                "ssyk_kompetensutveckling": res.get("ssykKompetensutveckling"),
+                "yrke_id_kompetensutveckling": res.get("yrkeIdKompetensutveckling"),
+                "onskat_yrke_ssyk": res.get("onskatYrkeSsyk"),
+                "onskat_yrke_id": res.get("onskatYrkeId"),
+                "validering_onskat_yrke": res.get("valideringOnskatYrke"),
+                "varaktighet1": res.get("varaktighet1"),
+                "varaktighet2": res.get("varaktighet2"),
+            }
+            for yrke in res.get("soktaYrken"):
+                yrke_dict = {
+                    "yrke_id": yrke.get("yrkeId"),
+                    "ssyk": yrke.get("ssyk"),
+                    "utbildning": yrke.get("utbildning"),
+                    "erfarenhet": yrke.get("erfarenhet")
+                }
+            for matchningsord in res.get("matchningsord"):
+                matchningsord_dict = {
+                    "taxonomyId": matchningsord.get("taxonomyId"),
+                    "matchningsord": matchningsord.get("matchningsord"),
+                    "typ": matchningsord.get("typ")
+                }
 
-            office = res.get("kontor", {}).get("kontorsKod")
-            if not db_values:
-                office = office and self.env["hr.department"].search_read(
-                    [("office_code", "=", office)], ["id"], limit=1
-                )
-                office = office and office[0]["id"] or None
-            else:
-                office = db_values["hr.department"].get(office, False)
-
-            sun = res.get("utbildning", {}).get("sunKod")
-            if not db_values:
-                sun = sun and self.env["res.sun"].search([("code", "=", sun)])
-                sun = sun or self.env["res.sun"].search([("code", "=", "999")]) or False
-                if sun:
-                    sun = sun["id"]
-            else:
-                sun = db_values["res.sun"].get(sun, db_values["res.sun"].get("999"))
-
-            skat = res.get("kontakt", {}).get("sokandekategoriKod")
-            if not db_values:
-                skat = skat and self.env["res.partner.skat"].search_read(
-                    [("code", "=", skat)], ["id"], limit=1
-                )
-                skat = skat and skat[0]["id"] or None
-            else:
-                skat = db_values["res.partner.skat"].get(skat, False)
-
-            user = res.get("kontor", {}).get("ansvarigHandlaggareSignatur")
-            if not db_values:
-                user = user and self.env["res.users"].search_read(
-                    [("login", "=", user)], ["id"], limit=1
-                )
-                user = user and user[0]["id"] or None
-            else:
-                user = db_values["res.users"].get(user, False)
-
-            segmenteringsval = res.get("segmentering", {}).get("segmenteringsval")
-            if segmenteringsval == "LOKAL":
-                registered_through = "local office"
-            elif segmenteringsval == "SJALVSERVICE":
-                registered_through = "self service"
-            elif segmenteringsval == "PDM":
-                registered_through = "pdm"
-            else:
-                registered_through = False
-
-            if res.get("medgivande", {}).get("infoTillArbetsgivare"):
-                share_info_with_employers = "True"
-            else:
-                share_info_with_employers = "False"
-
-            if res.get("medgivande", {}).get("paminnelseViaSms"):
-                sms_reminders = "True"
-            else:
-                sms_reminders = "False"
-            education_level = res.get("utbildning", {}).get("utbildningsniva")
-            if education_level is not None:
-                try:
-                    if not db_values:
-                        education_level = self.env[
-                            "res.partner.education.education_level"
-                        ].search([("name", "=", int(education_level))])
-                        if education_level:
-                            education_level = education_level.id
-                    else:
-                        education_level = db_values["education_level"].get(
-                            int(education_level), False
-                        )
-                except ValueError:
-                    education_level = False
-            else:
-                education_level = False
-
-            last_contact_type = res.get("kontakt", {}).get("senasteKontakttyp") or False
-            if last_contact_type:
-                last_contact_type = last_contact_type[0]
-
-            next_contact_type = (
-                res.get("kontakt", {}).get("nastaKontakttyper", {}) or False
-            )
-            jobseeker_dict = {
-                "firstname": res.get("arbetssokande", {}).get(
-                    "fornamn", "MISSING FIRSTNAME"
-                ),
-                "lastname": res.get("arbetssokande", {}).get(
-                    "efternamn", "MISSING LASTNAME"
-                ),
-                "customer_id": customer_id,
-                "social_sec_nr": res.get("arbetssokande", {}).get("personnummer"),
-                "customer_since": res.get("processStatus", {}).get("aktuellSedanDatum"),
-                "share_info_with_employers": share_info_with_employers,
-                "phone": res.get("kontaktuppgifter", {}).get("telefonBostad"),
-                "work_phone": res.get("kontaktuppgifter", {}).get("telefonArbetet"),
-                "mobile": res.get("kontaktuppgifter", {}).get("telefonMobil"),
-                "jobseeker_category_id": skat,
-                "deactualization_date": res.get("processStatus", {}).get(
-                    "avaktualiseringsDatum"
-                ),
-                "deactualization_reason": res.get("processStatus", {}).get(
-                    "avaktualiseringsOrsaksKod"
-                ),
-                "email": res.get("kontaktuppgifter", {}).get("epost"),
-                "office_id": office,
-                "state_id": state,
-                "registered_through": registered_through,
-                "user_id": user,
-                "sms_reminders": sms_reminders,
-                "next_contact_date": res.get("kontakt", {}).get("nastaKontaktdatum"),
-                "next_contact_time": res.get("kontakt", {}).get("nastaKontaktTid"),
-                "next_contact_type": next_contact_type,
-                "last_contact_date": res.get("kontakt", {}).get("senasteKontaktdatum"),
-                "last_contact_type": last_contact_type,
-                "is_jobseeker": True,
+            mask_keys = {
+                "arbetstid": "HELTID",
+                "arbetstidBeskrivning": "string",
+                "meriterFinns": "true",
+                "meriterUppdatDatum": "2020-07-22T08:39:27.300Z",
+                "flyttaVeckopendla": "true",
+                "sokerInomEu": "true",
+                "matchningsbar": "true",
+                "komplMatchningInfo": "string",
+                "korkort": "string",
+                "tillgangBil": "true",
+                "ssykKompetensutveckling": "string",
+                "yrkeIdKompetensutveckling": 0,
+                "onskatYrkeSsyk": "string",
+                "onskatYrkeId": 0,
+                "valideringOnskatYrke": "BEHOV_AV_FORDJUPAD_KOMPETENSKARTLAGGNING",
+                "varaktighet1": "ODEFINERAD",
+                "varaktighet2": "ODEFINERAD",
+                "soktaYrken": [
+                    {
+                        "yrkeId": 0,
+                        "ssyk": "string",
+                        "utbildning": "true",
+                        "erfarenhet": "true"
+                    }
+                ],
+                "soktaKommuner": [
+                    "string"
+                ],
+                "matchningsord": [
+                    {
+                        "taxonomyId": 0,
+                        "matchningsord": "string",
+                        "typ": "string"
+                    }
+                ]
             }
 
-            if sun and education_level:
-                if partner:
-                    edu_filtered = partner.education_ids.filtered(
-                        lambda e: (e.sun_id and e.sun_id.id == sun)
-                        and (
-                            e.education_level_id
-                            and e.education_level_id.id == education_level
-                        )
-                    )
-                    if edu_filtered:
-                        # Already exists. Do nothing.
-                        pass
-                    else:
-                        # delete education with source from AIS-F
-                        aisf_edu = partner.education_ids.filtered(lambda e: e.is_mask_matching)
-                        if aisf_edu:
-                            aisf_edu.unlink()
-                        # add empty list to jobseeker_dict to trigger next if
-                        jobseeker_dict["education_ids"] = []
-                else:
-                    jobseeker_dict["education_ids"] = []
-                if "education_ids" in jobseeker_dict:
-                    jobseeker_dict["education_ids"].append(
-                        (
-                            0,
-                            0,
-                            {
-                                "sun_id": sun,
-                                "education_level_id": education_level,
-                                "is_mask_matching": True,
-                            },
-                        )
-                    )
-
-            if partner:
-                partner.with_context(tracking_disable=True).write(jobseeker_dict)
-                create_update = "update"
-            else:
-                partner = (
-                    self.env["res.partner"]
-                    .with_context(tracking_disable=True, install_mode=True)
-                    .create(jobseeker_dict)
-                )
-                create_update = "create"
-
-            own_or_foreign_address_given = False
-            for address in res.get("kontaktuppgifter", {}).get("adresser", {}):
-                streetaddress = address.get("gatuadress")
-                if streetaddress:
-                    streetadress_array = streetaddress.split(",")
-                    if len(streetadress_array) == 1:
-                        street = streetadress_array[0]
-                        street2 = False
-                    elif len(streetadress_array) > 1:
-                        street = streetadress_array[1]
-                        street2 = streetadress_array[0]
-                    co_address = address.get("coAdress")
-                    zip = address.get("postnummer")
-                    city = address.get("postort")
-                    country = address.get("landsadress")
-                    if country:
-                        if not db_values:
-                            country = (
-                                self.env["res.country"]
-                                .with_context(lang="sv_SE")
-                                .search([("name", "ilike", country)])
-                            )
-                            country = country["id"] or None
-                        else:
-                            country = db_values["res.country"].get(country, False)
-                    if address.get("adressTyp") == "FBF":
-                        partner.address_co = co_address
-                        partner.street = street
-                        partner.street2 = street2
-                        partner.zip = zip
-                        partner.city = city
-                        partner.country_id = country
-                    elif (
-                        address.get("adressTyp") == "EGEN"
-                        or address.get("adressTyp") == "UTL"
-                    ):
-                        own_or_foreign_address_given = True
-                        given_address_object = self.env["res.partner"].search(
-                            [("parent_id", "=", partner.id)]
-                        )
-                        if not given_address_object:
-                            given_address_dict = {
-                                "address_co": co_address,
-                                "parent_id": partner.id,
-                                "street": street,
-                                "street2": street2,
-                                "zip": zip,
-                                "city": city,
-                                "type": "given address",
-                                "country_id": country,
-                            }
-                            self.env["res.partner"].create(given_address_dict)
-                        else:
-                            given_address_object.address_co = co_address
-                            given_address_object.street = street
-                            given_address_object.street2 = street2
-                            given_address_object.zip = zip
-                            given_address_object.city = city
-                            given_address_object.country_id = country
-            if not own_or_foreign_address_given:
-                given_address_object = self.env["res.partner"].search(
-                    [("parent_id", "=", partner.id)]
-                )
-                if given_address_object:
-                    given_address_object.unlink()
         except Exception:
             em = traceback.format_exc()
             log.log_message(
