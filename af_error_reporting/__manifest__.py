151d66114924969c8e1e697cdbdb171e72c20178
Adam Andersson <adam.x.andersson@arbetsformedlingen.se>
2021-10-12 14:33:01 +0200

    Added header button for error reporting.

---
+++ af_error_reporting/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Af Error Reporting",
+    "summary": "Arbetsförmedlingen error reporting for CRM",
+    "version": "12.0.0.1.0",
+    "category": "Utility",
+    "description": """
+		Arbetsförmedlingen error reporting for CRM.
+		v12.0.0.1.0 - AFC-2807 created module
+    """,
+    "author": "Arbetsförmedlingen",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'base',
+    ],
+    "data": [
+        'views/assets.xml',
+    ]
+}
