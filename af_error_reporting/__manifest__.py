584711a7fae4880a39730e300c8ee242fc75acfd
Adam <adam.andersson@icebyte.se>
2021-10-27 16:14:40 +0200

    Minor fixes.

--- af_error_reporting/__manifest__.py
+++ af_error_reporting/__manifest__.py
@@ -4,13 +4,14 @@
 {
     "name": "Af Error Reporting",
     "summary": "Arbetsförmedlingen error reporting for CRM",
-    "version": "12.0.0.1.1",
+    "version": "12.0.0.1.2",
     "category": "Utility",
     "description": """
     Jira
 ===================================================================== \n
     V12.0.0.1.0 AFC-2807: Added button for error reporting \n
-    V12.0.0.1.1 AFC-3002: Fixed problem when user opens link  \n
+    V12.0.0.1.1 AFC-2903: Added translation for module
+    V12.0.0.1.2 AFC-3002: Fixed problem when user opens link  \n
     """,
     "author": "Arbetsförmedlingen",
     "license": "LGPL-3",
