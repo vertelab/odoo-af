02573da54b52890fca1694a43a2c24fa5ff35059
Sebastian Gieser <sebastian.gieser@arbetsformedlingen.se>
2021-11-03 10:45:19 +0100

    af_error_reporting AFC-3041: now acts like web_a11y_report

--- af_error_reporting/__manifest__.py
+++ af_error_reporting/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Af Error Reporting",
     "summary": "Arbetsförmedlingen error reporting for CRM",
-    "version": "12.0.0.1.2",
+    "version": "12.0.0.2.0",
     "category": "Utility",
     "description": """
     Jira
@@ -12,14 +12,18 @@
     V12.0.0.1.0 AFC-2807: Added button for error reporting \n
     V12.0.0.1.1 AFC-2903: Added translation for module \n
     V12.0.0.1.2 AFC-3002: Fixed problem when user opens link  \n
+    V12.0.0.2.0 AFC-3041: rebuilt to work like accessibility report  \n
     """,
     "author": "Arbetsförmedlingen",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
-        'base',
+        'mail'
+    ],
+    "qweb": [
+        "static/src/xml/*.xml",
     ],
     "data": [
-        'views/assets.xml',
+        "views/assets.xml",
     ]
 }
